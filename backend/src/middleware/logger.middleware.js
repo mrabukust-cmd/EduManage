2c87dd46f7d526224ca32659de116dd558f28791
mrabukust-cmd <mrabukust@gmail.com>
2026-09-09 10:09:15 +0500

    feat(core-api): setup export service, express application entrypoints, and test suites

--- backend/src/middleware/logger.middleware.js
+++
@@ -1,7 +0,0 @@
-const morgan = require('morgan');
-const config = require('../config');
-
-// Use short format in test/production, dev format in development
-const logger = morgan(config.env === 'development' ? 'dev' : 'combined');
-
-module.exports = logger;
